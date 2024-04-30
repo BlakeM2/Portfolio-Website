70642cd708386fb0eb9805a4c6a956a2ed7d846d
BlakeM2 <148023918+BlakeM2@users.noreply.github.com>
2024-04-29 20:21:12 -0400

    update header and footer animation

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const observer = new IntersectionObserver((entries => {
+const observerHorizontal = new IntersectionObserver((entries => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.add("show");
+            entry.target.classList.add("show-horizontal");
         } else {
-            //entry.target.classList.remove("show");
+            //entry.target.classList.remove("show-horizontal");
         }
     });
 }));
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElements = document.querySelectorAll(".hidden-horizontal");
+hiddenElements.forEach((el) => observerHorizontal.observe(el));
+
+const observerVerticalTop = new IntersectionObserver((entries => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-vertical-top");
+        } else {
+            //entry.target.classList.remove("show-vertical-top");
+        }
+    });
+}));
+
+const hiddenVerticalTop = document.querySelectorAll(".hidden-vertical-top");
+hiddenVerticalTop.forEach((el) => observerVerticalTop.observe(el));
+
+const observerVerticalBottom = new IntersectionObserver((entries => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-vertical-bottom");
+        } else {
+            //entry.target.classList.remove("show-vertical-bottom");
+        }
+    });
+}));
+
+const hiddenVerticalBottom = document.querySelectorAll(".hidden-vertical-bottom");
+hiddenVerticalBottom.forEach((el) => observerVerticalBottom.observe(el));
 
